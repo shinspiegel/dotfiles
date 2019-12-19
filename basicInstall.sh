f50ae0444a22e6798233749f7e14735b3c5d57af
Shin <shin.jef@gmail.com>
2019-12-19 16:07:58 -0200

    Small typo fix

--- basicInstall.sh
+++ basicInstall.sh
@@ -38,10 +38,9 @@ ln -s /opt/mkcert/create-cert.sh ~/bin/create-cert
 
 #INSTALL SCRIPT FOR CLEANING BRANCHS
 sudo mkdir /opt/clean-branchs
-sudo wget https://raw.githubusercontent.com/shinspiegel/dotfiles/master/src/clean-branchs/create-cert.sh -O /opt/clean-branchs/clean-branchs.sh
+sudo wget https://raw.githubusercontent.com/shinspiegel/dotfiles/master/src/clean-branchs/clean-branchs.sh -O /opt/clean-branchs/clean-branchs.sh
 sudo chmod 755 /opt/clean-branchs/clean-branchs.sh
 ln -s /opt/clean-branchs/clean-branchs.sh ~/bin/clean-branchs
-
 
 
 
