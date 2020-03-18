c1a7c1ba39fdf16ede7e5119cf9e7cd4e21f8886
Shin <shin.jef@gmail.com>
2020-03-18 20:03:08 -0300

    update on godot script

--- src/godot.sh
+++ src/godot.sh
@@ -3,6 +3,6 @@
 #INSTALLING GODOT ENGINE
 sudo mkdir /opt/godot
 mkdir ~/Downloads/godot
-wget https://downloads.tuxfamily.org/godotengine/3.1.1/Godot_v3.1.1-stable_x11.64.zip -O ~/Downloads/godot/Godot_v3.1.1-stable_x11.64.zip
-sudo unzip ~/Downloads/godot/Godot_v3.1.1-stable_x11.64.zip -d /opt/godot
-ln -s /opt/godot/Godot_v3.1.1-stable_x11.64 ~/bin/godot
+wget https://downloads.tuxfamily.org/godotengine/3.2.1/Godot_v3.2.1-stable_x11.64.zip -O ~/Downloads/godot/Godot.zip
+sudo unzip ~/Downloads/godot/Godot.zip -d /opt/godot
+ln -s /opt/godot/Godot_v3.2.1-stable_x11.64 ~/bin/godot
