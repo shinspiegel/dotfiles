430f46a7c839c86503cf0bdb47036e4037c5ae6e
Shin <shin.jef@gmail.com>
2020-03-18 19:16:19 -0300

    More stuff

--- install.sh
+++ install.sh
@@ -1,11 +1,33 @@
 #! /bin/bash
 
-./newSrc/basic.sh
+chmod 755 ./newSrc/basic.sh
+chmod 755 ./newSrc/theming.sh
+chmod 755 ./newSrc/updateNode_and_nodeApps.sh
+chmod 755 ./newSrc/install_mkcerts.sh
+chmod 755 ./newSrc/install_otherApps.sh
+chmod 755 ./newSrc/ssh.sh
+chmod 755 ./newSrc/install_clean-branchs.sh
+chmod 755 ./newSrc/deno.sh
+chmod 755 ./newSrc/docker.sh
+chmod 755 ./newSrc/godot.sh
+chmod 755 ./newSrc/rust.sh
+chmod 755 ./newSrc/virtualMachineDependencies.sh
 
-# ./newSrc/deno.sh
-# ./newSrc/updateNode_and_nodeApps.sh
-# ./newSrc/install_mkcerts.sh
-# ./newSrc/install_clean-branchs.sh
-# ./newSrc/install_otherApps.sh
-# ./newSrc/virtualMachineDependencies.sh
+
+# This is the most basic stuff, enable the rest
+./newSrc/basic.sh
+./newSrc/theming.sh
+./newSrc/updateNode_and_nodeApps.sh
+./newSrc/install_mkcerts.sh
+./newSrc/install_otherApps.sh
+./newSrc/ssh.sh
+
+
+# Usefull stuff
+./newSrc/install_clean-branchs.sh
+./newSrc/deno.sh
+./newSrc/docker.sh
+./newSrc/godot.sh
+./newSrc/rust.sh
+./newSrc/virtualMachineDependencies.sh
 
--- newSrc/theming.sh
+++ newSrc/theming.sh
@@ -6,22 +6,25 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/maste
 curl -L git.io/antigen > antigen.zsh
 # or use git.io/antigen-nightly for the latest version
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-sudo wget https://github.com/powerline/fonts/raw/master/Inconsolata-g/Inconsolata-g%20for%20Powerline.otf -O /usr/share/fonts/Inconsolata-g-for-Powerline.otf
-wget https://raw.githubusercontent.com/shinspiegel/dotfiles/master/src/.zshrc -O ~/.zshrc
-source ~/.zshrc
+sudo wget https://github.com/shinspiegel/dotfiles/raw/master/newSrc/assets/fonts/Inconsolata-g-for-Powerline.otf -O /usr/share/fonts/Inconsolata-g-for-Powerline.otf
+sudo wget https://raw.githubusercontent.com/shinspiegel/dotfiles/master/src/.zshrc -O ~/.zshrc
+
 
 #INSTALLING FONTS FOR VISUAL CODE
-sudo wget https://github.com/tonsky/FiraCode/raw/master/distr/otf/FiraCode-Bold.otf -O /usr/share/fonts/FiraCode-Bold.otf
-sudo wget https://github.com/tonsky/FiraCode/raw/master/distr/otf/FiraCode-Light.otf -O /usr/share/fonts/FiraCode-Light.otf
-sudo wget https://github.com/tonsky/FiraCode/raw/master/distr/otf/FiraCode-Medium.otf -O /usr/share/fonts/FiraCode-Medium.otf
-sudo wget https://github.com/tonsky/FiraCode/raw/master/distr/otf/FiraCode-Regular.otf -O /usr/share/fonts/FiraCode-Regular.otf
-sudo wget https://github.com/tonsky/FiraCode/raw/master/distr/otf/FiraCode-Retina.otf -O /usr/share/fonts/FiraCode-Retina.otf
+sudo wget https://github.com/shinspiegel/dotfiles/raw/master/newSrc/assets/fonts/FiraCode-Bold.otf -O /usr/share/fonts/FiraCode-Bold.otf
+sudo wget https://github.com/shinspiegel/dotfiles/raw/master/newSrc/assets/fonts/FiraCode-Light.otf -O /usr/share/fonts/FiraCode-Light.otf
+sudo wget https://github.com/shinspiegel/dotfiles/raw/master/newSrc/assets/fonts/FiraCode-Medium.otf -O /usr/share/fonts/FiraCode-Medium.otf
+sudo wget https://github.com/shinspiegel/dotfiles/raw/master/newSrc/assets/fonts/FiraCode-Regular.otf -O /usr/share/fonts/FiraCode-Regular.otf
+sudo wget https://github.com/shinspiegel/dotfiles/raw/master/newSrc/assets/fonts/FiraCode-Retina.otf -O /usr/share/fonts/FiraCode-Retina.otf
+
 
 #DOWNLOADING MAC OS THEME
-git clone https://github.com/paullinuxthemer/McOS-Mint-Cinnamon-Edition.git ~/Downloads/macOS-theme
-mv ~/Downloads/macOS-theme/McOS-MJV-Cinnamon-Edition-2.0 ~/.themes/macOS_2
-rm -rf ~/Downloads/macOS-theme
-git clone https://github.com/shinspiegel/dotfiles.git ~/Downloads/macOS-icons
-cp ~/Downloads/macOS-icons/src/Cuppertine ~/.icons/CuppertineIcons -r
-rm -rf ~/Downloads/macOS-icons
+mkdir /home/$USER/.themes
+mkdir /home/$USER/.icons
 
+git clone https://github.com/shinspiegel/dotfiles.git ~/Downloads/theme_macOS
+
+cp ~/Downloads/macOS-icons/src/assets/icons ~/.themes/CuppertineIcons -r
+cp ~/Downloads/macOS-icons/src/assets/theme ~/.icons/CuppertineIcons -r
+
+rm -rf ~/Downloads/theme_macOS
